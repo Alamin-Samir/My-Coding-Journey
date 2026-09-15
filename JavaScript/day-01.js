e3a204c4d91b3cf961d5a47f74cbab09afa5ad88
samir <sa.samir213002019@gmail.com>
2026-09-15 06:23:26 +0600

    js basic code

---
+++ JavaScript/day-01.js
@@ -0,0 +1,88 @@
+
+
+// print console
+
+
+
+console.log("samir")
+
+//variable
+let name="samir"
+let age=24;
+console.log(name);
+console.log(age);
+
+//js addition sub , multi
+let a=10;
+let b=50;
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+
+
+//even or add
+
+let num=6;
+if(num%2===0){
+    console.log("even")
+}
+
+else{
+    print("odd")
+}
+
+// print 1 t0 5 number
+for (let i=1; i<=5; i++){
+    console.log(i)
+}
+
+// print a name with use function
+function greet(){
+    console.log("hello")
+}
+
+greet()
+
+// addition with function
+
+function add(a,b){
+    console.log(a+b)
+}
+add(7,9);
+
+//find a largest number
+
+let x=17;
+let y=27;
+
+if(x>y)
+{
+    console.log("x is largest number");
+
+}
+else{
+        console.log("y is largest nnumber");
+    }
+
+
+    //craete a array
+
+    let fruits=["apple","banana","orange","pinapple","jack-fruit","caster-apple"];
+
+    console.log(fruits);
+
+    // print a fixed iteam in array
+
+    console.log(fruits[1]);
+
+
+    //show array in loop
+
+for(let i=0; i<fruits.length; i++){
+
+    console.log(fruits[i]);
+}
+
+// create a object
