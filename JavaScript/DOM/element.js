ad9d39d4d3ebd335c167a984051fc0efaf3375e9
samir <sa.samir213002019@gmail.com>
2026-09-15 16:37:46 +0600

    create aelemt using a dom

---
+++ JavaScript/DOM/element.js
@@ -0,0 +1,27 @@
+
+let main=document.getElementById("main")
+
+
+let span=document.createElement("span")
+
+main.appendChild(span)
+span.textContent="hello span tag";
+
+
+let div1=document.createElement("div")
+
+document.body.appendChild(div1);
+
+div1.textContent="hello div 2";
+
+let p1=document.createElement("p")
+
+div1.appendChild(p1);
+
+p1.textContent="hello p1tag in div1";
+
+div1.className="p9"
+p1.id="p5"
+
+//create a list using DOM
+
